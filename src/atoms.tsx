92c6a91ad6a7c562e9651a57f89f128801e35632
SM0413 <kbjtmdals@naver.com>
2022-08-30 08:48:13 +0900

    Reordering part Two

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,13 +1,6 @@
-import { atom, selector } from "recoil";
+import { atom } from "recoil";
 
 export const toDoState = atom({
   key: "toDo",
   default: ["a", "b", "c", "d", "e", "f"],
-});
-
-export const toDoSelector = selector({
-  key: "toDoSelector",
-  get: ({ get }) => {
-    const toDos = get(toDoState);
-  },
 });
